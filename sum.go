2e386aadf5aa8bcb36750d1b77423132bda53458
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-01-16 01:06:59 -0300

    ci: check if quality gate is working on sonarcloud

--- sum.go
+++ sum.go
@@ -9,22 +9,22 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
 
-func times(a int, b int) int {
-	return a * b
-}
+// func times(a int, b int) int {
+// 	return a * b
+// }
 
-func sumX(a int, b int) int {
-	return a + b + a
-}
+// func sumX(a int, b int) int {
+// 	return a + b + a
+// }
 
-func subX(a int, b int) int {
-	return a - b - b
-}
+// func subX(a int, b int) int {
+// 	return a - b - b
+// }
 
-func div(a int, b int) int {
-	return a / b
-}
+// func div(a int, b int) int {
+// 	return a / b
+// }
